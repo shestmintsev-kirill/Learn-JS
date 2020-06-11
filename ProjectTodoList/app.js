bcd5673cf430c5e40f7be9402f530dcd20f2acfe
kirill <1kirillmihail1@gmail.com>
2020-06-11 20:47:17 +0300

    Final Todu with Theme

--- ProjectTodoList/app.js
+++ ProjectTodoList/app.js
@@ -38,6 +38,74 @@ const tasks = [
     return acc; // на каждой итерации получу 1 задачу под ключом _id
   }, {});
 
+  const themes = {
+    default: {
+      "--base-text-color": "#212529",
+      "--header-bg": "#007bff",
+      "--header-text-color": "#fff",
+      "--default-btn-bg": "#007bff",
+      "--default-btn-text-color": "#fff",
+      "--default-btn-hover-bg": "#0069d9",
+      "--default-btn-border-color": "#0069d9",
+      "--danger-btn-bg": "#dc3545",
+      "--danger-btn-text-color": "#fff",
+      "--danger-btn-hover-bg": "#bd2130",
+      "--danger-btn-border-color": "#dc3545",
+      "--input-border-color": "#ced4da",
+      "--input-bg-color": "#fff",
+      "--input-text-color": "#495057",
+      "--input-focus-bg-color": "#fff",
+      "--input-focus-text-color": "#495057",
+      "--input-focus-border-color": "#80bdff",
+      "--input-focus-box-shadow": "0 0 0 0.2rem rgba(0, 123, 255, 0.25)",
+    },
+    dark: {
+      "--base-text-color": "#212529",
+      "--header-bg": "#343a40",
+      "--header-text-color": "#fff",
+      "--default-btn-bg": "#58616b",
+      "--default-btn-text-color": "#fff",
+      "--default-btn-hover-bg": "#292d31",
+      "--default-btn-border-color": "#343a40",
+      "--default-btn-focus-box-shadow":
+        "0 0 0 0.2rem rgba(141, 143, 146, 0.25)",
+      "--danger-btn-bg": "#b52d3a",
+      "--danger-btn-text-color": "#fff",
+      "--danger-btn-hover-bg": "#88222c",
+      "--danger-btn-border-color": "#88222c",
+      "--input-border-color": "#ced4da",
+      "--input-bg-color": "#fff",
+      "--input-text-color": "#495057",
+      "--input-focus-bg-color": "#fff",
+      "--input-focus-text-color": "#495057",
+      "--input-focus-border-color": "#78818a",
+      "--input-focus-box-shadow": "0 0 0 0.2rem rgba(141, 143, 146, 0.25)",
+    },
+    light: {
+      "--base-text-color": "#212529",
+      "--header-bg": "#fff",
+      "--header-text-color": "#212529",
+      "--default-btn-bg": "#fff",
+      "--default-btn-text-color": "#212529",
+      "--default-btn-hover-bg": "#e8e7e7",
+      "--default-btn-border-color": "#343a40",
+      "--default-btn-focus-box-shadow":
+        "0 0 0 0.2rem rgba(141, 143, 146, 0.25)",
+      "--danger-btn-bg": "#f1b5bb",
+      "--danger-btn-text-color": "#212529",
+      "--danger-btn-hover-bg": "#ef808a",
+      "--danger-btn-border-color": "#e2818a",
+      "--input-border-color": "#ced4da",
+      "--input-bg-color": "#fff",
+      "--input-text-color": "#495057",
+      "--input-focus-bg-color": "#fff",
+      "--input-focus-text-color": "#495057",
+      "--input-focus-border-color": "#78818a",
+      "--input-focus-box-shadow": "0 0 0 0.2rem rgba(141, 143, 146, 0.25)",
+    },
+  };
+  let lastSelectedTheme = 'default'; //переменна для задания темы при отмене
+
   //* Elements UI
   const listContainer = document.querySelector(
     ".tasks-list-section",
@@ -46,11 +114,13 @@ const tasks = [
   const form = document.forms["addTask"]; //выбрал нужную форму с страницы
   const inputTitle = form.elements["title"]; // получить доступ к элементу формы (title)
   const inputBody = form.elements["body"]; // получаю элемент (body)
+  const themeSelect = document.getElementById("themeSelect"); //ищу select по id-themeSelect
 
   //*Events
   renderAllTasks(ObjofTasks); // вызов вывода задач на страницу
   form.addEventListener("submit", onFormSubmitHandler); //создал событие для формы (событие, обработчик)
   listContainer.addEventListener("click", onDeleteHandler); // создал событие для 'delete Task'(обработчик события на весь список, в котором генерируется задача)
+  themeSelect.addEventListener("change", onThemeSelectHandler); // событие на select
 
   function renderAllTasks(tasksList) {
     // вывод задач на страницу
@@ -79,9 +149,8 @@ const tasks = [
       "flex-wrap",
       "mt-2"
     );
-      //!делаю для обработчика удаления -->
-      li.setAttribute('data-task-id', _id) //получаем _id и сетим атрибут на 'li'//! теперь id есть в разметке и я могу получить к нему доступ
-
+    //!делаю для обработчика удаления -->
+    li.setAttribute("data-task-id", _id); //получаем _id и сетим атрибут на 'li'//! теперь id есть в разметке и я могу получить к нему доступ
 
     const span = document.createElement("span"); //теперь создаю внутри 'span'
     span.textContent = title; //задаю 'title' в качестве текста
@@ -135,27 +204,51 @@ const tasks = [
     return { ...newTask }; // для будущего использования возвращаю копию этой новой задачи(копию этой новой задачи получаю в переменной 'task')
   }
   //ф-ия буду вызывать из обработчика!
-  function deleteTask(id) { //создал ф-ию которая принимает id задачи, которую нужно удалить
-    const {title} = ObjofTasks[id]; //достал title и подставлю его в confirm ---->
+  function deleteTask(id) {
+    //создал ф-ию которая принимает id задачи, которую нужно удалить
+    const { title } = ObjofTasks[id]; //достал title и подставлю его в confirm ---->
     const isConfirm = confirm(`Вы хотите удалить задачу: ${title}?`); //будет спрашивать ч-з alert. После ответа будет true/false
     if (!isConfirm) return isConfirm; //если не подтвердил удаление(false) то возращаем c статусом false(return) и не делаем никаких задач
     delete ObjofTasks[id]; //если true то удаляем задачу
-    return isConfirm;// и возвращаю isConfirm с статусом true
+    return isConfirm; // и возвращаю isConfirm с статусом true
   }
-
-  function deleteTaskFromHtml(confirmed, el) { //вынес отдельно ф-ию для удаления( для дальнейшей разработки рекомендуется)
+  //ф-ия буду вызывать из обработчика!
+  function deleteTaskFromHtml(confirmed, el) {
+    //вынес отдельно ф-ию для удаления( для дальнейшей разработки рекомендуется)
     if (!confirmed) return; //если confirmed false то ничего не делаем
-    el.remove();//если confirmed true, то удаляем
+    el.remove(); //если confirmed true, то удаляем
   }
 
   function onDeleteHandler({ target }) {
     // вытягиваю из объекта event только target
     if (target.classList.contains("delete-btn")) {
       // проверка что бы таргет был именно на кнопке "Delete task"
-      const parent = target.closest('[data-task-id]') //  при клике буду искать ближайшего родителя с атрибутом 'data-task-id'
-      const id = parent.dataset.taskId;// забираю id через dataset, атрибут с приставкой data
-      const confirmed = deleteTask(id)//вызов ф-ии 'deleteTask', и запишу в переменную
+      const parent = target.closest("[data-task-id]"); //  при клике буду искать ближайшего родителя с атрибутом 'data-task-id'
+      const id = parent.dataset.taskId; // забираю id через dataset, атрибут с приставкой data
+      const confirmed = deleteTask(id); //вызов ф-ии 'deleteTask', и запишу в переменную
       deleteTaskFromHtml(confirmed, parent); //вызов ф-ии удаления
     }
+  }
+
+  function onThemeSelectHandler(e) {
+    //обработчик события изменения select
+    const selectedTheme = themeSelect.value; //в переменной будет храниться выбор темы
+    const isConfirm = confirm(
+      `Вы действительно хотите изменить тему: ${selectedTheme}`
+    ); // уточнение выбора чеоез глобальную 'isConfirm'
+    if (!isConfirm) {
+      themeSelect.value = lastSelectedTheme;//при отмене темы будет устанавливаться темаа ранее(lastSelectedTheme)
+    }; //если false то ничего не делатся
+    setTheme(selectedTheme); //вызов ф-ии установки темы
+    lastSelectedTheme = selectedTheme; //если true то тема(lastSelectedTheme) меняется на последнюю выюранную(selectedTheme)
+  }
+
+  function setTheme(name) {
+    // ф-ия, которая будет устанавливать тему, принимает название темы
+    const selectedThemObj = themes[name]; //из объекта вытягиваю нужный элемент
+    Object.entries(selectedThemObj).forEach(([key, value]) => {
+      //перебор переменных из 'themes'
+      document.documentElement.style.setProperty(key, value); //ч-з setProperty устанавливаю эти переменные
+    });
   }
 })(tasks);
